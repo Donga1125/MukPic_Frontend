671c0645944de08aff216cfe4f18f55b959d9485
pyan <pa98213@gmail.com>
2025-01-20 18:15:58 +0900

    fix: 이미지검색

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  reactStrictMode: true,
+  images: {
+    domains: ['mukpic-image.s3.ap-northeast-2.amazonaws.com'], // 이미지 호스트 추가
+  },
 };
 
 module.exports = nextConfig;
