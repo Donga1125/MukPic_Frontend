ccc00184c01da80ab9a959afd3a31689cd2457d4
pyan <pa98213@gmail.com>
2025-01-26 01:29:38 +0900

    change: 버튼 디자인 수정

--- app/(pages)/info/page.tsx
+++ app/(pages)/info/page.tsx
@@ -143,7 +143,7 @@ function InfoPageContent() {
 
           <div className="mt-8 flex justify-center space-x-4">
             <button
-              className="px-6 py-3 bg-[#CFF7CA] text-black font-bold rounded-md"
+              className="bg-gray-800 text-white font-semibold px-5 py-2 rounded-full"
               onClick={handleConfirmClick}
             >
               OK
